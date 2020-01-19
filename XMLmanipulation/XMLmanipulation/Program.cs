9725a18f24c360c8a920a79694cfd8ceacd47b08
Bruktawit <37048244+Bruktawit@users.noreply.github.com>
2020-01-19 23:03:33 +0300

    Add files via upload

---
+++ XMLmanipulation/XMLmanipulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml;
+
+namespace XMLmanipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("students.xml");
+            foreach(XmlNode node in doc.DocumentElement)
+            {
+                
+                foreach(XmlNode child in node.ChildNodes)
+                {
+                    Console.WriteLine(child.InnerText);
+                }
+            }
+        }
+    }
+}
